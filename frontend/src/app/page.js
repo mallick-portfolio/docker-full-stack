acb328718f5e98538afd9f1b11fa1b54841f2f5c
mallick-portfolio <tamal1714019@gmail.com>
2024-08-30 12:56:03 +0600

    start docker django next js project

---
+++ frontend/src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita natus
+        nisi reiciendis! Voluptatem rerum sint nostrum ipsa magni ex animi
+        expedita quam aperiam modi. Tempora molestias ipsa voluptatibus sunt et?
+      </div>
+      <Button className="text-secondary">Click me</Button>
+    </main>
+  );
+}
